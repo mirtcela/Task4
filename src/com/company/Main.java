ab5596885fc05996430c4f1d7a719b268c1ba091
mirtcela <mirtcela@yandex.ru>
2020-08-19 19:52:15 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите двоичное число: ");
+        //способ 1 - встроенные методы;
+        String bin = in.nextLine();
+        int int1= Integer.parseInt(bin,2);
+        System.out.printf("Десятичное число: %d \n", int1);
+        //способ 2 - c использованием поразрядной операции;
+        char[] bin_arr = bin.toCharArray();
+        int int2 = 0;
+        for (int i =0 ; i <= bin_arr.length-1; i++){
+            int2+=(bin_arr[i]-'0') << (bin_arr.length-1-i);
+        }
+        System.out.printf("Десятичное число: %d \n", int2);
+    }
+}
