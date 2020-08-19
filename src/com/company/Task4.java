89470333395c6aa2887995123018d336cf936810
mirtcela <mirtcela@yandex.ru>
2020-08-19 19:56:03 +0300

    -rename class

--- src/com/company/Task4.java
+++ src/com/company/Task4.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Scanner;
 
-public class Main {
+public class Task4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите двоичное число: ");
